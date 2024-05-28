ecb999a4394f235295123ff729639eba7100e178
yadukrishnan <yadhu4507@gmail.com>
2024-05-28 14:16:47 +0530

    course edit functionality added

--- src/pages/admin/Addcourse.jsx
+++ src/pages/admin/Addcourse.jsx
@@ -1,36 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../components/Headers";
 import { addcourseSchema } from "../../utilities/Validation";
-import { Formik, Form, Field, ErrorMessage } from 'formik'; // Import Formik components and ErrorMessage
+import { Formik, Form, Field, ErrorMessage } from "formik"; // Import Formik components and ErrorMessage
 import axiosInstance from "../../../instance/axiosInstance";
 import { useNavigate, useParams } from "react-router-dom";
 function Addcourse() {
   const navigate = useNavigate();
-  const initialValues = {
-    Coursename: '',
-    fee: '',
-    Description: '',
-    teachername: ''
-  };
-
-  
+  const [courseDetails, setCourseDetails] = useState({
+    Coursename: "",
+    fee: "",
+    Description: "",
+    teachername: "",
+  });
   const { courseId } = useParams();
-  useEffect(()=>{
-   if(courseId){
-  const editCourse =()=>{
+  useEffect(() => {
+    if (courseId) {
+      const editCourse = async () => {
+        const response = await axiosInstance.get(
+          `/admin/editcourse?courseId=${courseId}`
+        );
+        const { data, status } = response;
+        const courseData = data.courseData;
+        setCourseDetails(courseData);
+      };
+      editCourse();
+    }
+  }, []);
 
-  }
-   }
-  },[])
-
-  const handlesubmit =async (values) => {
-    const datas = values
-    const response =await axiosInstance.post('/admin/addcourse', datas)
-    const{data}=response
-    if(data.success === true){
+  const handlesubmit = async (values) => {
+    const datas = values;
+    
+   if(!courseId){
+    const response = await axiosInstance.post("/admin/addcourse", datas);
+    const { data } = response;
+    if (data.success === true) {
       navigate("/admin/courselist");
     }
-  };
+  }else{
+    const response = await axiosInstance.put(`/admin/editcourse?courseId=${courseId}`, datas);
+    const { data } = response;
+    if (data.success === true) {
+      navigate("/admin/courselist");
+    }
+  }
+   }
 
   return (
     <>
@@ -42,11 +55,14 @@ function Addcourse() {
               <h1 className="text-[1.5rem] font-thin">Add course</h1>
             </div>
             <Formik
-              initialValues={initialValues}
+              initialValues={courseDetails}
+              enableReinitialize={true}
               validationSchema={addcourseSchema}
               onSubmit={handlesubmit}
             >
-              {({ isSubmitting, errors, touched }) => ( // Destructure props correctly
+              {(
+                { isSubmitting, errors, touched } // Destructure props correctly
+              ) => (
                 <Form className="flex flex-col gap-2">
                   <Field
                     type="text"
@@ -54,15 +70,23 @@ function Addcourse() {
                     className="px-3 shadow-sm border border-[#00000015] focus:border-blue-600 outline-none w-96 h-10"
                     placeholder="Coursename"
                   />
-                  <ErrorMessage name="Coursename" component="small" className="text-red-400" />
-                  
+                  <ErrorMessage
+                    name="Coursename"
+                    component="small"
+                    className="text-red-400"
+                  />
+
                   <Field
                     type="text"
                     name="fee"
                     className="px-3 shadow-sm border border-[#00000015] focus:border-blue-600 outline-none w-96 h-10"
                     placeholder="fee"
                   />
-                  <ErrorMessage name="fee" component="small" className="text-red-400" />
+                  <ErrorMessage
+                    name="fee"
+                    component="small"
+                    className="text-red-400"
+                  />
 
                   <Field
                     type="text"
@@ -70,7 +94,11 @@ function Addcourse() {
                     className="px-3 shadow-sm border border-[#00000015] focus:border-blue-600 outline-none w-96 h-10"
                     placeholder="Description"
                   />
-                  <ErrorMessage name="Description" component="small" className="text-red-400" />
+                  <ErrorMessage
+                    name="Description"
+                    component="small"
+                    className="text-red-400"
+                  />
 
                   <Field
                     type="text"
@@ -78,12 +106,16 @@ function Addcourse() {
                     className="px-3 shadow-sm border border-[#00000015] focus:border-blue-600 outline-none w-96 h-10"
                     placeholder="Teachername"
                   />
-                  <ErrorMessage name="teachername" component="small" className="text-red-400" />
+                  <ErrorMessage
+                    name="teachername"
+                    component="small"
+                    className="text-red-400"
+                  />
 
                   <button
                     type="submit"
                     className="px-3 shadow-sm border border-[#00000015] outline-none w-96 h-10 bg-blue-500 text-white flex items-center justify-center gap-2"
-                    disabled={isSubmitting} 
+                    disabled={isSubmitting}
                   >
                     Add course
                   </button>
